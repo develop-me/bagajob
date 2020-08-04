72b9d16997c6936aec4ab31322cb57f0b90c8d2d
Kieran Cott <kmp.cott@gmail.com>
2020-08-04 18:22:55 +0100

    correct stylesheet location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/styles.css';
+import '../src/css/styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
