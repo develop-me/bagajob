fbf464f95eca99634d813956d23e7c1a688509ea
Kieran Cott <kmp.cott@gmail.com>
2020-08-12 13:41:38 +0100

    remove bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.min.css';
-import './bootstrap/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
