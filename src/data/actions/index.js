6a7de0322f60c1aa09f18c2d173a9906fb8b2e6f
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-18 20:50:47 +0100

    user actions created in their own folder within the actions folder, index file in the actions folder exports all actions

--- src/data/actions/index.js
+++ src/data/actions/index.js
@@ -1,5 +1,11 @@
-export * from './listJobsActions'
-export * from './editJobActions'
-export * from './viewJobActions'
-export * from './addJobActions'
-export * from './deleteJobActions'
+// job actions
+export * from './JobActions/listJobsActions'
+export * from './JobActions/editJobActions'
+export * from './JobActions/viewJobActions'
+export * from './JobActions/addJobActions'
+export * from './JobActions/deleteJobActions'
+// user actions
+export * from './UserActions/editUserActions'
+export * from './UserActions/loginUserActions'
+export * from './UserActions/logoutUserActions'
+export * from './UserActions/registerUserActions'
