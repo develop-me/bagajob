8c91e41cb872098f103182e6b335d345c7f48699
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-18 20:31:52 +0100

    user constants created in their own folder, index file exports them in the constants folder

--- src/data/constants/index.js
+++ src/data/constants/index.js
@@ -1,5 +1,10 @@
-export * from './listJobsConstants'
-export * from './editJobConstants'
-export * from './viewJobConstants'
-export * from './addJobConstants'
-export * from './deleteJobConstants'
+// job constants
+export * from './JobConstants/listJobsConstants'
+export * from './JobConstants/editJobConstants'
+export * from './JobConstants/viewJobConstants'
+export * from './JobConstants/addJobConstants'
+export * from './JobConstants/deleteJobConstants'
+// user constants
+export * from './UserConstants/loginUserConstants'
+export * from './UserConstants/logoutUserConstants'
+export * from './UserConstants/registerUserConstants'
