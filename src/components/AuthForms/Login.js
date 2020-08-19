0cc38c76d5a613cc040346cda039ab6c96f9e599
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-19 16:11:21 +0100

    login form updated to use relevant atomic design classes from components.html

--- src/components/AuthForms/Login.js
+++ src/components/AuthForms/Login.js
@@ -18,12 +18,14 @@ const Login = ({ history }) => {
             email: email,
             password: password,
         }
-        console.log(state)
+
         dispatch(loginUser(state, history));
     }
 
+    // brings in authResponse global state property
     const authResponse = useSelector(state => state.authResponse)
 
+    // resets authResponse every time component renders
     useEffect(() => {
         dispatch(resetAuthResponse());
     }, []);
@@ -31,29 +33,40 @@ const Login = ({ history }) => {
     return (
         <>
             <h1 className="brand_text">bagajob</h1>
-            <form onSubmit={handleSubmit} className="login_form">
-                <div className="login_form_input_container">
-                    <img className="email_icon" src={email_icon} alt="an @ symbol" />
-                    <input
-                        value={email}
-                        placeholder="Email address"
-                        type="text"
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <img className="password_icon" src={lock_icon} alt="a padlock symbol" />
-                    <input
-                        value={password}
-                        placeholder="Password"
-                        type="password"
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <small className="password_forgot">Forgot password?</small>
-                </div>
-                <button className="primarybtn">LOG IN</button>
-            </form>
-            <b>{authResponse !== null && authResponse}</b>
+            <div class="login_container">
+                <form class="login_form" onSubmit={handleSubmit}>
+                    <div class="login_form_input_container">
+                        <span><img class="email_icon" src={email_icon} alt="email-icon" /></span>
+                        <input
+                            value={email}
+                            type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Email address"
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div class="login_form_input_container">
+                        <span><img class="password_icon" src={lock_icon} alt="password-icon" /></span>
+                        <input
+                            value={password}
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <p class="password_forgot">Forgot password?</p>
+                    <button class="primarybtn" type="submit" id="login_btn">
+                        LOG IN
+                    </button>
+                    <button class="secondarybtn" type="submit" id="signup_btn">
+                        SIGN UP
+                    </button>
+                    <b>{authResponse !== null && authResponse}</b>
+                </form>
+            </div>
         </>
     );
 };
