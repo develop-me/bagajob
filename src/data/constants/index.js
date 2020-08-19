85d76e27d78a69419740b8889cd38e76cd3d5465
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-19 14:58:36 +0100

    user constants moved to one single auth constants file as it's easier to see them all in one place

--- src/data/constants/index.js
+++ src/data/constants/index.js
@@ -4,22 +4,5 @@ export * from './JobConstants/editJobConstants'
 export * from './JobConstants/viewJobConstants'
 export * from './JobConstants/addJobConstants'
 export * from './JobConstants/deleteJobConstants'
-// user constants
-export * from './UserConstants/loginUserConstants'
-export * from './UserConstants/logoutUserConstants'
-export * from './UserConstants/registerUserConstants'
-export * from './UserConstants/editUserConstants'
-
-export const RESET_AUTH_RESPONSE = 'RESET_AUTH_RESPONSE'
-
-export const LOADING = 'LOADING'
-
-export const SHORT_PASSWORD = 'SHORT_PASSWORD'
-
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-
-export const LOGIN_ERROR = 'LOGIN_ERROR'
-
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
-
-export const SIGNUP_ERROR = 'SIGNUP_ERROR'
+// auth constants
+export * from './AuthConstants'
