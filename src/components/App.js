bc54805ddabaf3ee043c9115cd42b6f28517d973
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-19 11:43:14 +0100

    app.js moved to components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
           BagaJob
         </h1>
