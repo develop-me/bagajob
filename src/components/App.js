90f23a53d2b437c0689e1f69a6b8074e9b66a8de
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-20 15:54:48 +0100

    add job form skeleton built and imported into app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './AuthForms/Login';
 import SignUp from './AuthForms/SignUp';
+import AddJobForm from './AddJobForm';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="app">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route path="/home/login" component={Login} />
           <Route path="/home/signup" component={SignUp} />
+          <Route path="/addjob" component={AddJobForm} />
         </Switch>
       </div>
     </BrowserRouter>
