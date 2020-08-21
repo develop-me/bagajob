cd973eeb7bd237206399bd45840c7c52b4d596c5
Kieran Cott <kmp.cott@gmail.com>
2020-08-21 13:09:26 +0100

    undo persist state changes

--- src/data/store.js
+++ src/data/store.js
@@ -1,20 +1,20 @@
 import reducer from "./reducers";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-// import persistState from "redux-localstorage";
+import persistState from "redux-localstorage";
 
 const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//     reducer,
-//     composeEnhancers(applyMiddleware(thunk), persistState()),
-// );
-
 const store = createStore(
     reducer,
-    // initial,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk), persistState()),
 );
+
+// const store = createStore(
+//     reducer,
+//     initial,
+//     composeEnhancers(applyMiddleware(thunk))
+// );
 
 export default store;
