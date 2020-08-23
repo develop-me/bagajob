16986345ad081e618d04a0f0456d15294b87e545
Kieran Cott <kmp.cott@gmail.com>
2020-08-23 17:46:22 +0100

    add fonts and buttons to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,22 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <>
-            <div>HOME</div>
-            <Link to="/home/login">LOGIN</Link>
-            <Link to="/home/signup">SIGN UP</Link>
+            <h1 className="brand-title">bagajob</h1>
+            <h3 className="brand-text">
+                Track job applications from start to finish
+            </h3>
+            <div className="button-container">
+                <Link to="/home/login">
+                    <button className="primarybtn login-btn-home" type="submit" id="">
+                        LOG IN
+                    </button>
+                    </Link>
+                <Link to="/home/signup">
+                    <button className="secondarybtn signup-btn-home" type="submit" id="">
+                        SIGN UP
+                    </button>  
+                </Link>
+            </div>
         </>
     );
 };
