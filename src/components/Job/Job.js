1bce1f45e23f26f59450344742906d21ac03f296
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-01 11:15:26 +0100

    loading component and job id imported and destructured into job component respectively

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getSingleJob } from '../../data/Jobs/actions'
+import Loading from '../Loading'
 import JobDetailsCard from './JobDetailsCard'
 import ApplicationCard from './ApplicationCard'
 import InterviewCard from './InterviewCard'
 
-const Job = () => {
-
+const Job = ({ jobId }) => {
     // brings in single job fetched from database
-    const job = useSelector(state => state.job)
+    const { job, loaded } = useSelector(state => state)
 
-    return (
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getSingleJob(jobId));
+    }, []);
+
+    return !loaded ? <Loading /> : (
         <div style={{ display: "flex", width: "70%", margin: "0 auto", justifyContent: "space-between" }}>
             <JobDetailsCard job={job} />
             <ApplicationCard job={job} />
             <InterviewCard job={job} />
         </div>
-    );
+    )
 };
 
 export default Job;
