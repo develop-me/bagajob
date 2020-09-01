31712a4eebe7c41d4db18786dbff9e2a6be01748
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-01 11:27:04 +0100

    react modal brought into main page component for pop up add job form

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import { useSelector } from 'react-redux'
+import { Modal } from 'react-responsive-modal'
 import JobPreview from './JobPreview'
+import JobForm from '../JobForms/JobForm'
 
 const MainPage = () => {
+    const [open, setOpen] = useState(false)
+
+    const openModal = () => {
+        setOpen(true)
+    }
+
+    const closeModal = () => {
+        setOpen(false)
+    }
 
     // brings in jobs global state property
     const jobs = useSelector(state => state.jobs)
@@ -12,10 +23,11 @@ const MainPage = () => {
         <>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <a>Sort by</a>
-                <Link to="/addjob">Add new application</Link>
-                <h1>My Applications</h1>
-            </div>
+                {/* <Link to="/addjob">Add new application</Link> */}
+                <button onClick={openModal}>Add new application</button>
 
+            </div>
+            <h1>My Applications</h1>
             <div>
                 {jobs.map((job, index) => (
                     job.active ?
@@ -24,6 +36,10 @@ const MainPage = () => {
                         null
                 ))}
             </div>
+            <Modal open={open} onClose={closeModal} center>
+                {/* <JobForm /> */}
+                <p>hellooo</p>
+            </Modal>
         </>
     );
 };
