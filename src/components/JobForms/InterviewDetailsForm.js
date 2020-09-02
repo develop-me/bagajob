f1fc9ac236bbb01b4d7866a0d2d4e55d6f0aedee
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-02 11:43:51 +0100

    props updated in interview details form component

--- src/components/JobForms/InterviewDetailsForm.js
+++ src/components/JobForms/InterviewDetailsForm.js
@@ -1,27 +1,55 @@
 import React from 'react';
 
-const InterviewDetailsForm = ({ prevStep, nextStep }) => {
-    return (
-        <div>
+const InterviewDetailsForm = ({
+    currentStep,
+    prevStep,
+    handleSubmit,
+    values,
+    handleChange,
+    handleInterviewFormat
+}) => {
+    return currentStep && (
+        <>
             <h1>Step 3: Interview Details</h1>
             <h3>Add any interview details. No interview yet? Just leave this blank and edit later.</h3>
-            <label htmlFor="interviewDate">Interview date</label>
-            <input type="date" id="interviewDate" />
+            <label htmlFor="interview_date">Interview date</label>
+            <input
+                type="date"
+                id="interview_date"
+                value={values.interview_date}
+                onChange={handleChange}
+            />
 
-            <label htmlFor="interviewFormat">Interview format</label>
-            <select name="interviewFormat" id="interviewFormat">
+            <label htmlFor="interview_format">Interview format</label>
+            <select
+                id="interview_format"
+                value={values.interview_format}
+                onChange={handleInterviewFormat}
+            >
                 <option value="person">In person</option>
                 <option value="phone">Telephone</option>
                 <option value="online">Online</option>
             </select>
 
-            <label htmlFor="interviewerName">Interview name</label>
-            <input type="text" id="interviewerName" />
+            <label htmlFor="interviewer">Interview name</label>
+            <input
+                type="text"
+                id="interviewer"
+                value={values.interviewer}
+                onChange={handleChange}
+            />
 
-            <textarea placeholder="Interview notes" cols="30" rows="10"></textarea>
+            <textarea
+                cols="30"
+                rows="10"
+                placeholder="Interview notes"
+                id="interview_notes"
+                value={values.interview_notes}
+                onChange={handleChange}
+            />
             <button onClick={prevStep}>Go back</button>
-            <button onClick={nextStep}>Finish &amp; Save</button>
-        </div>
+            <button type="submit" onClick={handleSubmit}>Finish &amp; Save</button>
+        </>
     );
 };
 
