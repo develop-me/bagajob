0c3a62ea1ca6028df1c82a5cec501b1e1bc6e344
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-02 11:49:16 +0100

    add new job action updated to take job data object and user id

--- src/data/Jobs/actions.js
+++ src/data/Jobs/actions.js
@@ -10,20 +10,9 @@ import {
 
 // store bearer token in redux state, fetch it using useSelector(), look into jwt refresh tokens
 export const addNewJob = data => dispatch => {
+    const { user_id, job_data } = data
     const token = localStorage.getItem('user')
-    axios.post("user/jobs", {
-        job_title: data.jobTitle,
-        company: data.company,
-        description: data.jobDescription,
-        salary: data.salary,
-        location: data.location,
-        date_applied: data.dateApplied,
-        closing_date: data.closingDate,
-        recruiter_name: data.recruiterName,
-        recruiter_email: data.recruiterEmail,
-        recruiter_phone: data.recruiterPhone,
-        stage: data.stage,
-    }, {
+    axios.post(`user/${user_id}/jobs`, job_data, {
         headers: {
             'Authorization': token
         }
