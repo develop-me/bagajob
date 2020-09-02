b645780da898583feccc131b8373fe1ea33c8b38
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-02 11:42:16 +0100

    onRequestClose prop added to job form modal and ternary updated to display active job previews

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,44 +1,42 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom"
 import { useSelector } from 'react-redux'
-import { Modal } from 'react-responsive-modal'
+import Modal from 'react-modal'
 import JobPreview from './JobPreview'
 import JobForm from '../JobForms/JobForm'
 
 const MainPage = () => {
-    const [open, setOpen] = useState(false)
+    const [showModal, setOpen] = useState(false)
+    const jobs = useSelector(state => state.job.jobs)
 
+    // opens job form modal
     const openModal = () => {
         setOpen(true)
     }
 
+    // closes jobs form modal
     const closeModal = () => {
         setOpen(false)
     }
-
-    // brings in jobs global state property
-    const jobs = useSelector(state => state.jobs)
 
     return (
         <>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <a>Sort by</a>
-                {/* <Link to="/addjob">Add new application</Link> */}
                 <button onClick={openModal}>Add new application</button>
 
             </div>
             <h1>My Applications</h1>
             <div>
                 {jobs.map((job, index) => (
-                    job.active ?
-                        <JobPreview job={job} key={index}></JobPreview>
-                        :
-                        null
+                    job.active && <JobPreview job={job} key={index} />
                 ))}
             </div>
-            <Modal open={open} onClose={closeModal} center>
-                {/* <JobForm /> */}
-                <p>hellooo</p>
+            <Modal
+                isOpen={showModal}
+                onClose={closeModal}
+                onRequestClose={closeModal}
+            >
+                <JobForm />
             </Modal>
         </>
     );
