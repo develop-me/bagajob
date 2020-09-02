af036746bd5d175380797f759833b1c50f5d512e
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-02 15:21:40 +0100

    application note comments and function names updated

--- src/components/Job/ApplicationCard/ApplicationNote.js
+++ src/components/Job/ApplicationCard/ApplicationNote.js
@@ -7,12 +7,13 @@ const ApplicationNote = ({ applicationNote }) => {
     let [note, setNote] = useState(applicationNote.note_data)
 
     const dispatch = useDispatch()
-    // the necessary ids to make updateInterview put request
+    // the necessary ids to make updateAppNote patch request
     const { id: user_id } = useSelector(state => state.auth.user)
     const { id: job_id } = useSelector(state => state.job.job)
     const { id: note_id } = applicationNote
 
-    const submitNote = e => {
+    // updates single application card note
+    const handleUpdate = e => {
         e.preventDefault()
 
         dispatch(updateAppNote({
@@ -23,6 +24,7 @@ const ApplicationNote = ({ applicationNote }) => {
         }))
     }
 
+    // deletes single application card note
     const handleDelete = () => {
         dispatch(deleteAppNote({
             user_id,
@@ -32,26 +34,24 @@ const ApplicationNote = ({ applicationNote }) => {
     }
 
     return (
-        <form onSubmit={e => submitNote(e)}>
-            {
-                editing ?
-                    <textarea
-                        cols="30"
-                        rows="10"
-                        id="note"
-                        value={note}
-                        onChange={e => setNote(e.target.value)
-                        }
-                    />
-                    :
-                    <p>{note} {applicationNote.note_date}</p>
+        <form onSubmit={handleUpdate}>
+            {editing ?
+                <textarea
+                    cols="30"
+                    rows="10"
+                    id="note"
+                    value={note}
+                    onChange={e => setNote(e.target.value)
+                    }
+                />
+                :
+                <p>{note} {applicationNote.note_date}</p>
             }
 
-            {
-                editing ?
-                    <button type="submit" onClick={() => setEditing(false)}>Save</button>
-                    :
-                    <button onClick={() => setEditing(true)}>Edit</button>
+            {editing ?
+                <button type="submit" onClick={() => setEditing(false)}>Save</button>
+                :
+                <button onClick={() => setEditing(true)}>Edit</button>
             }
             <button onClick={handleDelete}>Delete</button>
         </form >
