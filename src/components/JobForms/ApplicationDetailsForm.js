5c8181f987d2e0da120f03475340eb981151574a
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-02 11:42:51 +0100

    props updated in application details form component

--- src/components/JobForms/ApplicationDetailsForm.js
+++ src/components/JobForms/ApplicationDetailsForm.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-const ApplicationDetailsForm = ({ prevStep, values, nextStep }) => {
-    return (
-        <div>
+const ApplicationDetailsForm = ({
+    currentStep,
+    prevStep,
+    values,
+    nextStep,
+    handleChange
+}) => {
+    return currentStep && (
+        <>
             <h1>Step 2: Application</h1>
             <h3>Here you can upload your CV, Cover Letter and any application notes.</h3>
-            <label htmlFor="myFile">Upload my CV</label>
-            <input type="file" id="myFile" name="filename" />
-            <label htmlFor="myFile">Upload my Cover Letter</label>
-            <input type="file" id="myFile" name="filename" />
-            <textarea placeholder="Application notes" cols="30" rows="10"></textarea>
+            <label htmlFor="cv">Upload my CV</label>
+            <input type="file" id="cv" />
+            <label htmlFor="cover_letter">Upload my Cover Letter</label>
+            <input type="file" id="cover_letter" />
+            <textarea
+                cols="30"
+                rows="10"
+                placeholder="Application notes"
+                id="application_notes"
+                value={values.application_notes}
+                onChange={handleChange}
+
+            />
             <button onClick={prevStep}>Go back</button>
             <button onClick={nextStep}>Next step</button>
-        </div>
+        </ >
     );
 };
 
