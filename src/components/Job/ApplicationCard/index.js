69015d9ca9a1033a6eaaa43fc736ed040e759b34
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-05 19:25:59 +0100

    functions and props updated in job parent component

--- src/components/Job/ApplicationCard/index.js
+++ src/components/Job/ApplicationCard/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ApplicationInput from './ApplicationInput'
-import { addNewAppNote } from '../../../data/AppNotes/actions'
+import { addAppNote } from '../../../data/AppNotes/actions'
 import ApplicationNote from './ApplicationNote'
 
 const ApplicationCard = ({ job }) => {
-    const { id: user_id } = useSelector(state => state.auth.user)
+    const { id: user_id } = useSelector(state => state.user)
     const { id: job_id } = job
     const dispatch = useDispatch()
 
     // adds new empty note in application card
-    const addNote = () => {
-        dispatch(addNewAppNote({
+    const handleAddAppNote = () => {
+        dispatch(addAppNote({
             user_id,
             job_id
         }))
@@ -22,16 +22,15 @@ const ApplicationCard = ({ job }) => {
             <div style={{ display: "flex", flexDirection: "column", border: "1px solid black", padding: "2rem" }}>
                 <h1 className="para_header">Application</h1>
                 <ApplicationInput
-                    appInput={job.cv}
+                    appCardInput={job.cv}
                     jobProperty="cv"
-                    job={job}
                 />
                 <ApplicationInput
-                    appInput={job.cover_letter}
+                    appCardInput={job.cover_letter}
                     jobProperty="cover_letter"
-                    job={job}
                 />
-                <button onClick={addNote}>Add note</button>
+                <h3>Notes</h3>
+                <button onClick={handleAddAppNote}>Add note</button>
                 {
                     job.application_notes.map(note => {
                         return (
@@ -41,7 +40,7 @@ const ApplicationCard = ({ job }) => {
                 }
             </div>
         </>
-    );
-};
+    )
+}
 
-export default ApplicationCard;
+export default ApplicationCard
