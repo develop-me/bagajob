84ac3e775adb7f624f9d9e0144c935d0442a15aa
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-05 19:21:46 +0100

    quotation marks updated in sign up component

--- src/components/AuthForms/SignUp.js
+++ src/components/AuthForms/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { resetAuthResponse, signUp } from '../../data/Auth/actions'
 
