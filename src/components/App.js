91f49198192cffe1e2084599e31334811a7b5aee
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-06 14:43:28 +0100

    routes amended in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,8 @@ function App() {
       <div className="app">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/home" component={Home} />
-          <Route path="/home/login" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/home/login" component={Login} />
           <Route path="/home/signup" component={SignUp} />
           <Route path="/mainpage" component={MainPage} />
           <Route path="/account" component={Account} />
