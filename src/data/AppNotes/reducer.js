e68a6bc064c4f1472c4cd1dcc12b5ce95bf4a124
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-06 14:44:45 +0100

    application note reducer updated

--- src/data/AppNotes/reducer.js
+++ src/data/AppNotes/reducer.js
@@ -1,15 +1,23 @@
 import {
     APPLICATION_NOTE_POST_REQUEST,
     APPLICATION_NOTE_POST_SUCCESS,
+    APPLICATION_NOTE_POST_FAILURE,
     APPLICATION_NOTE_PATCH_REQUEST,
     APPLICATION_NOTE_PATCH_SUCCESS,
+    APPLICATION_NOTE_PATCH_FAILURE,
     APPLICATION_NOTE_DELETE_REQUEST,
-    APPLICATION_NOTE_DELETE_SUCCESS
+    APPLICATION_NOTE_DELETE_SUCCESS,
+    APPLICATION_NOTE_DELETE_FAILURE
 } from './constants'
 
 export default (state, action) => {
     const { type, payload } = action
     switch (type) {
+        case APPLICATION_NOTE_POST_REQUEST:
+            return {
+                ...state,
+                loaded: false
+            }
         case APPLICATION_NOTE_POST_SUCCESS:
             return {
                 ...state,
@@ -18,6 +26,17 @@ export default (state, action) => {
                     application_notes: [...state.application_notes, payload]
                 },
                 loaded: true
+            }
+        case APPLICATION_NOTE_POST_FAILURE:
+            return {
+                ...state,
+                loaded: true,
+                errors: payload
+            }
+        case APPLICATION_NOTE_PATCH_REQUEST:
+            return {
+                ...state,
+                loaded: false
             }
         case APPLICATION_NOTE_PATCH_SUCCESS:
             return {
@@ -28,16 +47,33 @@ export default (state, action) => {
                 },
                 loaded: true
             }
+        case APPLICATION_NOTE_PATCH_FAILURE:
+            return {
+                ...state,
+                loaded: true,
+                errors: payload
+            }
+        case APPLICATION_NOTE_DELETE_REQUEST:
+            return {
+                ...state,
+                loaded: false
+            }
         case APPLICATION_NOTE_DELETE_SUCCESS:
             return {
                 ...state,
                 job: {
-                    ...state.job.job,
+                    ...state.job,
                     application_notes: state.job.application_notes.filter(note => note.id !== payload)
                 },
                 loaded: true
             }
+        case APPLICATION_NOTE_DELETE_FAILURE:
+            return {
+                ...state,
+                loaded: true,
+                errors: payload
+            }
         default:
-            return {}
+            return state
     }
 }
