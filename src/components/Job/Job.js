384b348ae90297d85bcfc251fedc2b9b7654a4c7
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-06 20:07:27 +0100

    job id extracted from props in job component to be used in single job get request

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -6,13 +6,18 @@ import JobDetailsCard from './JobDetailsCard'
 import ApplicationCard from './ApplicationCard'
 import InterviewCard from './InterviewCard'
 
-const Job = ({ job_id }) => {
-    const { job, loaded } = useSelector(state => state)
+const Job = ({ match }) => {
+    const { job, loaded, user: { id: user_id } } = useSelector(state => state)
     const dispatch = useDispatch()
+    const job_id = match.params.id
 
     // single job is fetched when component renders
     useEffect(() => {
-        dispatch(getSingleJob(job_id));
+        const data = {
+            user_id,
+            job_id
+        }
+        dispatch(getSingleJob(data));
     }, []);
 
     return !loaded ? <Loading /> : (
