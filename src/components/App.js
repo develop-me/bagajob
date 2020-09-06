8ec00cfaa1dffcd764802706a8947363ac56dd56
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-06 20:01:17 +0100

    private route component created and imported into app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
 import Home from './Home'
 import Login from './AuthForms/Login'
 import SignUp from './AuthForms/SignUp'
@@ -7,24 +8,20 @@ import MainPage from './MainPage/MainPage'
 import Job from './Job/Job'
 import Account from './Account/Account'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/home/login" component={Login} />
-          <Route path="/home/signup" component={SignUp} />
-          <Route path="/mainpage" component={MainPage} />
-          <Route path="/account" component={Account} />
-          <Route path="/jobs/:id" render={({ match }) => (
-            <Job jobId={match.params.id} />
-          )} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/home/login" component={Login} />
+        <Route path="/home/signup" component={SignUp} />
+        <PrivateRoute path="/mainpage" component={MainPage} />
+        <PrivateRoute path="/account" component={Account} />
+        <PrivateRoute path="/jobs/:id" component={Job} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
 
 export default App
