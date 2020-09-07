b1952fce16e5cc97e71b1e10d6260580e0b88727
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-07 10:14:40 +0100

    validate auth helper function updated, can now be used for email input in forgot password component

--- src/helpers/validateAuth.js
+++ src/helpers/validateAuth.js
@@ -1,6 +1,8 @@
 const validateAuth = values => {
     let errors = {}
-    // name errors - this first if statement allows this function to be used for the login form, which doesn't have a name input. otherwise this error would occur for the login form and stop it submitting
+    // the nested if statements allow this function to be used for the login form, signup form and password reset form, as they check whether there is a name and password input present. For forms they are not present in, they will be ignored, rather than causing an error saying they haven't been filled out.
+
+    // name errors 
     if ('name' in values) {
         if (!values.name) {
             errors.name = "Please enter a name."
@@ -11,10 +13,12 @@ const validateAuth = values => {
         errors.email = "Please enter a valid email address."
     }
     // password errors
-    if (!values.password) {
-        errors.password = "Please enter a password."
-    } else if (values.password.length < 6) {
-        errors.password = "Password must be at least 6 characters."
+    if ('password' in values) {
+        if (!values.password) {
+            errors.password = "Please enter a password."
+        } else if (values.password.length < 6) {
+            errors.password = "Password must be at least 6 characters."
+        }
     }
     return errors
 }
