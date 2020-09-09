db033aed110f088adeee9dab3fd525e1397c38bd
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-09 10:59:16 +0100

    password reset component included in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from './Home'
 import Login from './AuthForms/Login'
 import SignUp from './AuthForms/SignUp'
 import ForgotPassword from './AuthForms/ForgotPassword'
+import PasswordReset from './AuthForms/PasswordReset'
 import MainPage from './MainPage/MainPage'
 import Job from './Job/Job'
 import Account from './Account/Account'
@@ -12,19 +13,18 @@ import FourOhFour from './FourOhFour'
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/home/login" component={Login} />
-        <Route exact path="/home/signup" component={SignUp} />
-        <Route path="/home/forgot-password" component={ForgotPassword} />
-        <PrivateRoute path="/mainpage" component={MainPage} />
-        <PrivateRoute path="/jobs/:id" component={Job} />
-        <PrivateRoute path="/account" component={Account} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/home/login" component={Login} />
+      <Route exact path="/home/signup" component={SignUp} />
+      <Route path="/home/forgot-password" component={ForgotPassword} />
+      <Route path="/home/password-reset/:token" component={PasswordReset} />
+      <PrivateRoute path="/mainpage" component={MainPage} />
+      <PrivateRoute path="/jobs/:id" component={Job} />
+      <PrivateRoute path="/account" component={Account} />
+      <Route component={FourOhFour} />
+    </Switch>
   </BrowserRouter>
 )
 
