f413bdaec7479727c837b139d9c5ebcb8b493c20
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-11 09:13:11 +0100

    application note updated, edit functionality still needs work

--- src/components/Job/ApplicationCard/ApplicationNote.js
+++ src/components/Job/ApplicationCard/ApplicationNote.js
@@ -4,7 +4,7 @@ import { updateAppNote, deleteAppNote } from '../../../data/AppNotes/actions'
 
 const ApplicationNote = ({ applicationNote }) => {
     const [editing, setEditing] = useState(true)
-    const [note, setNote] = useState(applicationNote.note_data)
+    const [note, setNote] = useState(applicationNote.data)
     const dispatch = useDispatch()
     // the necessary ids to make updateAppNote patch request
     const { id: user_id } = useSelector(state => state.user)
@@ -21,8 +21,6 @@ const ApplicationNote = ({ applicationNote }) => {
             note_id,
             note_data: note
         }))
-
-        setEditing(false)
     }
 
     // deletes single application card note
@@ -35,25 +33,23 @@ const ApplicationNote = ({ applicationNote }) => {
     }
 
     return (
-        <form onSubmit={handleUpdateAppNote}>
+        <>
             {editing ?
-                <textarea
-                    cols="30"
-                    rows="10"
-                    value={note}
-                    onChange={e => setNote(e.target.value)}
-                />
+                <form onSubmit={handleUpdateAppNote}>
+                    < textarea
+                        cols="30"
+                        rows="10"
+                        value={note}
+                        onChange={e => setNote(e.target.value)}
+                    />
+                    <button type="submit" onClick={setEditing(false)}>Save</button>
+                </form >
                 :
-                <p>{note} {applicationNote.note_date}</p>
+                <p>{note} {applicationNote.date}</p>
             }
-
-            {editing ?
-                <button type="submit" onClick={() => setEditing(false)}>Save</button>
-                :
-                <button onClick={() => setEditing(true)}>Edit</button>
-            }
+            {!editing && <button onClick={() => setEditing(true)}>Edit</button>}
             <button onClick={handleDeleteAppNote}>Delete</button>
-        </form >
+        </>
     )
 }
 
