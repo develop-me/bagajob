4e6dfc694e055dcc38eaaabba2d39ca3732713b1
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-11 09:17:04 +0100

    interview axios routes updated

--- src/data/ApiRequests/interviews.js
+++ src/data/ApiRequests/interviews.js
@@ -5,7 +5,7 @@ const token = localStorage.getItem('user')
 // interview post request
 export const addInterview = data => {
     const { user_id, job_id } = data
-    return axios.post(`user/${user_id}/job/${job_id}/interviews`, null, {
+    return axios.post(`user/${user_id}/jobs/${job_id}/interviews`, null, {
         headers: {
             'Authorization': token
         }
@@ -17,7 +17,7 @@ export const updateInterview = data => {
     const { user_id, job_id, interview_data } = data
     // only destructure the id as we want to preserve the interview_data object to send through to the db
     const { id: interview_id } = interview_data
-    return axios.patch(`user/${user_id}/job/${job_id}/interviews/${interview_id}`, interview_data, {
+    return axios.patch(`user/${user_id}/jobs/${job_id}/interviews/${interview_id}`, interview_data, {
         headers: {
             'Authorization': token
         }
@@ -27,7 +27,7 @@ export const updateInterview = data => {
 // interview delete request
 export const deleteInterview = data => {
     const { user_id, job_id, interview_id } = data
-    return axios.delete(`user/${user_id}/job/${job_id}/interviews/${interview_id}`, null, {
+    return axios.delete(`user/${user_id}/jobs/${job_id}/interviews/${interview_id}`, null, {
         headers: {
             'Authorization': token
         }
