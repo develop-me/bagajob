97a40b3b925abf51348e4d785538cd9273f26b40
Adam Gardner <adam.gardner4991@gmail.com>
2020-09-11 09:15:59 +0100

    axios routes updated in appnotes api file

--- src/data/ApiRequests/appNotes.js
+++ src/data/ApiRequests/appNotes.js
@@ -5,7 +5,7 @@ const token = localStorage.getItem('user')
 // application note post request
 export const addAppNote = data => {
     const { user_id, job_id } = data
-    return axios.post(`user/${user_id}/job/${job_id}/app_notes`, null, {
+    return axios.post(`user/${user_id}/jobs/${job_id}/app-notes`, null, {
         headers: {
             'Authorization': token
         }
@@ -15,7 +15,7 @@ export const addAppNote = data => {
 // application note patch request
 export const updateAppNote = data => {
     const { user_id, job_id, note_id, note_data } = data
-    return axios.patch(`user/${user_id}/job/${job_id}/app_notes/${note_id}`, note_data, {
+    return axios.patch(`user/${user_id}/jobs/${job_id}/app-notes/${note_id}`, note_data, {
         headers: {
             'Authorization': token
         }
@@ -25,7 +25,7 @@ export const updateAppNote = data => {
 // application note delete request
 export const deleteAppNote = data => {
     const { user_id, job_id, note_id } = data
-    return axios.delete(`user/${user_id}/job/${job_id}/app_notes/${note_id}`, null, {
+    return axios.delete(`user/${user_id}/jobs/${job_id}/app-notes/${note_id}`, null, {
         headers: {
             'Authorization': token
         }
