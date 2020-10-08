170d301076fe8a310018197bded035bf28e4710b
kierancott <kmp.cott@gmail.com>
2020-10-08 15:15:19 +0100

    remove comment

--- src/data/ApiRequests/interviews.js
+++ src/data/ApiRequests/interviews.js
@@ -13,7 +13,6 @@ export const addInterview = data => {
 // interview patch request
 export const updateInterview = data => {
     const { user_id, job_id, interview_data, interview_id, access_token } = data
-    // only destructure the id as we want to preserve the interview_data object to send through to the db
     return axios.patch(`user/${user_id}/jobs/${job_id}/interviews/${interview_id}`, interview_data, {
         headers: {
             'Authorization': `Bearer ${access_token}`
