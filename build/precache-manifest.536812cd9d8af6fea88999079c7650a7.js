d0b3548d9b24315f7b09da2b660338c3f73829ff
Nik Osvalds <nosvalds@gmail.com>
2020-10-10 10:48:28 +0100

    add static files from build folder for deployment to tracking

---
+++ build/precache-manifest.536812cd9d8af6fea88999079c7650a7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975ac3f996ca4945115ebaa309fe877d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951c3825766b34428cb9",
+    "url": "/static/css/main.c5e96f01.chunk.css"
+  },
+  {
+    "revision": "b2cf25562995c056e2a5",
+    "url": "/static/js/2.6719ec92.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.6719ec92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951c3825766b34428cb9",
+    "url": "/static/js/main.66fb9637.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "2e26a9163cb4974dcba1bea5107d4492",
+    "url": "/static/media/Lato-BlackItalic.2e26a916.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "f98d18040a766b7bc4884b8fcc154550",
+    "url": "/static/media/Lato-BoldItalic.f98d1804.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "4d80ac573c53d192dafd99fdd6aa01e9",
+    "url": "/static/media/Lato-LightItalic.4d80ac57.ttf"
+  },
+  {
+    "revision": "9ccb885c9cf8e503d557f15e0b2cbf24",
+    "url": "/static/media/Merriweather-Bold.9ccb885c.ttf"
+  },
+  {
+    "revision": "63e1f06ea6bf31c8f3143c143e7e6c2a",
+    "url": "/static/media/Merriweather-BoldItalic.63e1f06e.ttf"
+  },
+  {
+    "revision": "498bf4ee4ac9fab22ad9f814839173b2",
+    "url": "/static/media/Merriweather-Italic.498bf4ee.ttf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "3a6c2396c3088125e7d3e4dd62be47c7",
+    "url": "/static/media/add-task-24px.3a6c2396.svg"
+  },
+  {
+    "revision": "ce59afd41fb167f23ae964d926b0e7c8",
+    "url": "/static/media/done-24px.ce59afd4.svg"
+  },
+  {
+    "revision": "6186b4f41f3834e89a263fe061d07ddf",
+    "url": "/static/media/email_icon.6186b4f4.svg"
+  },
+  {
+    "revision": "1c126a7de9e525d0767aa883f86d3e6f",
+    "url": "/static/media/lock_icon.1c126a7d.svg"
+  },
+  {
+    "revision": "77caa0acd86d0367308b5beca11457c4",
+    "url": "/static/media/timeline-24px.77caa0ac.svg"
+  },
+  {
+    "revision": "a1b47ba453cf2607a986b4a1628f1d29",
+    "url": "/static/media/work-24px.a1b47ba4.svg"
+  }
+]);
