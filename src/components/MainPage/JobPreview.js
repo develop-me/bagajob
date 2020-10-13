f21b581e1eb952fc66e4889fe4fff2fbc407cf79
kierancott <kmp.cott@gmail.com>
2020-10-13 13:10:52 +0100

    style mainpage xs screens

--- src/components/MainPage/JobPreview.js
+++ src/components/MainPage/JobPreview.js
@@ -23,16 +23,18 @@ const JobPreview = ({ job }) => {
 
     return (
         <>
-             <div className=  {!smallScreen ? "job-card-container" : "job-card-container-small"}>
+            <div className=  {!smallScreen ? "job-card-container" : "job-card-container-small"}>
 
                 { smallScreen ?                     
    
                 <div className="job-card-text-small">
                     <p className="job-card-text-bold">{job.title}</p><p className="job-card-at"> at</p><p className="job-card-text-bold">{job.company}</p>
-                </div> : null
-                }
+                </div>
+                :
+                null }
 
-                <div className={smallScreen ? "job-card-flex-container-small" : "job-card-flex-container" }>
+            <div className={smallScreen ? "job-card-flex-container-small" : "job-card-flex-container" }>
+
                 { !smallScreen ? 
                 <div className="job-card-text">
                         <h4 className="para-highlight">
@@ -43,48 +45,50 @@ const JobPreview = ({ job }) => {
                         </h4>
                     </div>
                 : null }
-
-                <div className="status-bar">
-                    <StageTracker
-                        handleClick={() => setCurrent("1")}
-                        selected={current === "1"}
-                        label="Not applied"
-                        value="1"
-                        title={title}
-                        company={company}
-                        active={active}
-                        job_id={job.id}
-                    />
-                    <StageTracker
-                        handleClick={() => setCurrent("2")}
-                        selected={current === "2"}
-                        label="Applied"
-                        value="2"
-                        title={title}
-                        company={company}
-                        active={active}
-                        job_id={job.id}
-                    />
-                    <StageTracker
-                        handleClick={() => setCurrent("3")}
-                        selected={current === "3"}
-                        label="Interview"
-                        value="3"
-                        title={title}
-                        company={company}
-                        active={active}
-                        job_id={job.id}
-                    />
-                    <StageTracker
-                        handleClick={() => setCurrent("4")}
-                        selected={current === "4"}
-                        label="Offer"
-                        value="4"
-                        title={title}
-                        company={company}
-                        active={active}
-                        job_id={job.id}
-                    />
+                
+                <div className="status-bar-container">
+                    <div className="status-bar">
+                        <StageTracker
+                            handleClick={() => setCurrent("1")}
+                            selected={current === "1"}
+                            label="Not applied"
+                            value="1"
+                            title={title}
+                            company={company}
+                            active={active}
+                            job_id={job.id}
+                        />
+                        <StageTracker
+                            handleClick={() => setCurrent("2")}
+                            selected={current === "2"}
+                            label="Applied"
+                            value="2"
+                            title={title}
+                            company={company}
+                            active={active}
+                            job_id={job.id}
+                        />
+                        <StageTracker
+                            handleClick={() => setCurrent("3")}
+                            selected={current === "3"}
+                            label="Interview"
+                            value="3"
+                            title={title}
+                            company={company}
+                            active={active}
+                            job_id={job.id}
+                        />
+                        <StageTracker
+                            handleClick={() => setCurrent("4")}
+                            selected={current === "4"}
+                            label="Offer"
+                            value="4"
+                            title={title}
+                            company={company}
+                            active={active}
+                            job_id={job.id}
+                        />
+                    </div>
                 </div>
                 <Link to={`/jobs/${job.id}`}><img className="expand-job-btn" src={ require('../../assets/images/arrow_right.svg') }/></Link>
             </div>
